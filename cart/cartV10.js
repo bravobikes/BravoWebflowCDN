ba9d1684b7d454274fbe00e017af40abe6e4b3ea
Andres Montemayor <andresmontemayor@Andress-MacBook-Pro-2.local>
2023-03-02 18:18:01 -0600

    if statement values uppercase

---
+++ cart/cartV10.js
@@ -0,0 +1,112 @@
+LIST_OF_SIZEBIKE.forEach( (e) => {
+    e.addEventListener('change', (event) => {
+        CuadroRuta = event.id
+});
+})
+
+
+$('input[name="manubrio"]').click(function(){
+    manubrioFinal = this.value;
+
+});
+//Grupo
+$('input[name="groupset"]').click( function(){
+    groupsetFinal = this.value;
+
+});   
+
+//Rines
+$('input[name="rines"]').click( function(){
+    rinesFinal = this.value;
+});       
+
+// Click on Checkout button
+$('#irACheckout').click(function(){
+    if (frame_set_chk == true){
+        console.log("checkbox true")
+
+        if (CuadroRuta == "XS"){
+            // xs
+            $('.add-to-cart-button-xs').trigger('click');
+        } else if (CuadroRuta == "S"){
+            // s
+            $('.add-to-cart-button-s').trigger('click');
+        } else if (CuadroRuta == "M") {
+            // m
+            $('.add-to-cart-button-m').trigger('click');
+        } else if (CuadroRuta == "ML") {
+            // ml
+            $('.add-to-cart-button-ml').trigger('click');
+        } else if (CuadroRuta == "L") {
+            // l
+            $('.add-to-cart-button-l').trigger('click');
+        } else if (CuadroRuta == "XL"){
+            // xl
+            $('.add-to-cart-button-xl').trigger('click');
+        }
+       
+    } else {
+        console.log("checkbox false")
+        if (CuadroRuta == "XS"){
+            // xs
+            $('.add-to-cart-button-xs').trigger('click');
+        } else if (CuadroRuta == "S"){
+            // s
+            $('.add-to-cart-button-s').trigger('click');
+        } else if (CuadroRuta == "M") {
+            // m
+            $('.add-to-cart-button-m').trigger('click');
+        } else if (CuadroRuta == "ML") {
+            // ml
+            $('.add-to-cart-button-ml').trigger('click');
+        } else if (CuadroRuta == "L") {
+            // l
+            $('.add-to-cart-button-l').trigger('click');
+        } else if (CuadroRuta == "XL"){
+            // xl
+            $('.add-to-cart-button-xl').trigger('click');
+        }
+
+            
+       
+                if (groupsetFinal == 0) {
+                    if (rinesFinal == 0){
+                        if (manubrioFinal == 0) {
+                            $('.add-to-cart-g1lama').trigger('click');
+                        } else if (manubrioFinal == 1) {
+                            $('.add-to-cart-g1lamc').trigger('click');
+                        }
+
+                    } else if (rinesFinal == 1){
+                        if (manubrioFinal == 0) {
+                            $('.add-to-cart-g1lcma').trigger('click');
+                        } else if (manubrioFinal == 1) {
+                            $('.add-to-cart-g1lcmc').trigger('click');
+                        }
+                    }  
+                        
+                } 
+                // else if (groupsetFinal == 1) {
+                //     if (rinesFinal == 0){
+                       
+                //     } else if (rinesFinal == 1){
+
+                //     }      
+
+                // }
+
+            
+        // Add Finish
+        
+        // Add Engravings
+    }
+})
+
+// Remove items from cart
+$('#close-cart-button').click(function() {
+	$('.w-commerce-commercecartitem a').each(function() {
+    $(this).click(function() {
+           this.click();
+        }).click()
+  });
+})
