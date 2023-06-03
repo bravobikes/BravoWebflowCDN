6b8a91ee69461080d7f53736d0b228bb82b119cd
bravobikes <c.ramirez@bravobikes.cc>
2023-06-02 21:40:10 -0600

    name change

--- translate/translateV1.js
+++ translate/translateV1.js
@@ -1,3 +1,8 @@
+// js deliver link:
+// <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+
+
 function googleTranslateElementInit() {
     new google.translate.TranslateElement({pageLanguage: 'es', layout: google.translate.TranslateElement.FloatPosition.TOP_LEFT}, 'google_translate_element');
   }
