1f00601fc31db5c4c4acdb7c097a29f28a250e4f
bravobikes <c.ramirez@bravobikes.cc>
2023-06-04 19:46:03 -0600

    pruebas merge fecha con cart

--- preciosYPesos/preciosYPesos60.js
+++ preciosYPesos/preciosYPesos60.js
@@ -4,6 +4,9 @@ const innerPeso = document.querySelector('#innerPeso')
 const innerPrecioM = document.querySelector('#innerPrecioM')
 const innerPesoM = document.querySelector('#innerPesoM')
 
+const fechaEntrega = document.getElementById("fechaEntrega")
+const fechaEntregaMobile = document.getElementById("fechaEntregaMobile")
+
 const LIST_OF_SIZEBIKE = Object.values($('input[name="sizeBike"]')).splice(0,6)
 const LIST_OF_GROUPSET = Object.values($('input[name="groupset"]')).splice(0,2)
 const LIST_OF_RINES = Object.values($('input[name="rines"]')).splice(0,2)
@@ -12,6 +15,17 @@ const LIST_OF_BASE = Object.values($('input[name="base"]')).splice(0,2)
 const LIST_OF_DISENO = Object.values($('input[name="diseno"]')).splice(0,3)
 
 let frame_set_chk = false
+ 
+let grupoEntrega = 0
+let ruedosEntrega = 0
+let manubrioEntrega = 0
+
+//Declaracion de fecha actual
+const newDate = new Date
+
+//Declaracion de array
+const arrayMeses = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
 
 /* PESOS */
 
@@ -220,3 +234,68 @@ function SetPeso(){
     innerPeso.innerText = bubble/1000 + ' Kg'
     innerPesoM.innerText = bubble/1000 + ' Kg'
 }
+
+
+
+
+
+
+
+fechaEntrega.innerHTML = ((arrayMeses[newDate.getMonth() + 2] + ' ' + newDate.getFullYear()));
+fechaEntregaMobile.innerHTML = ((arrayMeses[newDate.getMonth() + 2] + ' ' + newDate.getFullYear()));
+console.log((arrayMeses[newDate.getMonth()] + ' ' + newDate.getFullYear()))
+
+function getDelivery() {
+
+    let month = newDate.getMonth()
+    let year = newDate.getFullYear()
+    month = month + 2
+    month = month + grupoEntrega + ruedosEntrega + manubrioEntrega
+
+    if(month>11){
+        month = month - 11 
+        year +=1
+    }
+    
+    var day = (arrayMeses[month] + " " + year)
+
+    fechaEntrega.innerText = day
+    fechaEntregaMobile.innerText = day
+    
+
+    console.log(day)
+}
+
+
+
+$('#105').click(function(){
+    grupoEntrega = 0
+    getDelivery()
+  });
+
+$('#ultegra').click(function(){
+    grupoEntrega = 1
+    getDelivery()
+  });
+
+$('#aluminio').click(function(){
+    ruedosEntrega = 0
+    getDelivery()
+  
+  });  
+
+  $('#carbon').click(function(){
+    ruedosEntrega = 1
+    getDelivery()
+  
+  });  
+
+  $('#standard').click(function(){
+    manubrioEntrega = 0
+    getDelivery()
+  });  
+
+  $('#aero').click(function(){
+    manubrioEntrega = 1
+    getDelivery()
+  });
